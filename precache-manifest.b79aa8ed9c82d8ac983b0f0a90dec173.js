04a5df3ecb737918322079327ceff0bd7b83e27d
German <german.alzate970814@gmail.com>
2018-12-05 02:42:29 -0500

    First commit

---
+++ precache-manifest.b79aa8ed9c82d8ac983b0f0a90dec173.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3f8d2bbc635d9fd2cd2",
+    "url": "/static/css/main.62c867a1.chunk.css"
+  },
+  {
+    "revision": "e3f8d2bbc635d9fd2cd2",
+    "url": "/static/js/main.e3f8d2bb.chunk.js"
+  },
+  {
+    "revision": "7bc206dfd7986066243a",
+    "url": "/static/css/1.53e530db.chunk.css"
+  },
+  {
+    "revision": "7bc206dfd7986066243a",
+    "url": "/static/js/1.7bc206df.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "62677451fab24bd568cd60d2443e08de",
+    "url": "/static/media/loader.62677451.svg"
+  },
+  {
+    "revision": "cfc1c6249641db6fb2f6c8e91c0e90c0",
+    "url": "/index.html"
+  }
+];
